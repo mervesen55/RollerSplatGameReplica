dd48dbd784819977bc079be71d2ac76a712be8af
Merve Şensoy <merveesensoy@hotmail.com>
2020-12-12 23:24:44 +0300

    Add

---
+++ Roller Splat/Assets/Scipts/BallController.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    public Rigidbody rb;   
+    private Vector2 firstPressPos;
+    private Vector2 secondPressPos;
+    private Vector2 currentSwipe;
+
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Swipe();
+    }
+    void OnCollisionEnter(Collision collision)
+        {
+        
+            if (collision.collider.tag == "ground")
+            {
+                collision.transform.gameObject.GetComponent<Renderer>().material.color = Color.green;
+                GameManager.instance.uncoloredTiles -= 1;
+                Debug.Log(GameManager.instance.uncoloredTiles);
+                collision.collider.tag = "coloredTiles";
+            if (GameManager.instance.uncoloredTiles == 0)
+                GameManager.instance.setLevel();
+            }
+        }
+
+    public void Swipe()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            
+            firstPressPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            
+            secondPressPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+
+           
+            currentSwipe = new Vector2(secondPressPos.x - firstPressPos.x, secondPressPos.y - firstPressPos.y);
+
+           
+            currentSwipe.Normalize();
+
+            
+            if (currentSwipe.y > 0 &&  currentSwipe.x > -0.5f && currentSwipe.x < 0.5f)
+        {
+                Debug.Log("up swipe");
+                rb.velocity = new Vector3(0, 0, 200);
+               
+            }
+            
+            if (currentSwipe.y < 0 && currentSwipe.x > -0.5f && currentSwipe.x < 0.5f)
+        {
+                Debug.Log("down swipe");
+                rb.velocity = new Vector3(0, 0, -200);
+                
+               
+            }
+            
+            if (currentSwipe.x < 0 && currentSwipe.y > -0.5f && currentSwipe.y < 0.5f)
+        {
+                Debug.Log("left swipe");
+                rb.velocity = new Vector3(-200, 0, 0);
+                
+            }
+          
+            if (currentSwipe.x > 0 && currentSwipe.y > -0.5f &&  currentSwipe.y < 0.5f)
+        {
+                
+                rb.velocity = new Vector3(200, 0, 0);
+                
+            }
+        }
+    }
+
+ 
+
+ 
+}
---
+++ Roller Splat/Assets/Scipts/GameManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    public GameObject[] Level;
+    public int uncoloredTiles = 10; // colored tiles number
+    private int i = 0; //level iterator
+    private int TileCounter = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+    }
+
+    public void setLevel()
+    {
+
+        {
+            Debug.Log("Level completed");
+            Debug.Log(i);
+            Level[i].SetActive(false);
+            Level[i + 1].SetActive(true);
+            TileCounter = 0;
+            SetUnColoredTiles();
+            if (i < 4)
+                i++;
+        }
+    }
+
+    public void SetUnColoredTiles()
+    {
+        var tiles = GameObject.FindGameObjectsWithTag("ground");
+
+        for (var i = 0; i < tiles.Length; i++)
+        {
+            TileCounter++;
+        }
+
+        uncoloredTiles = TileCounter;
+
+    }
+
+}
